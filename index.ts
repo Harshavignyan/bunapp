ab92af53da7a950b872ba94e18352037c5fbe177
Harshavignyan <ahvignyan@gmail.com>
2024-09-03 11:18:19 +0530

    request server

--- index.ts
+++ index.ts
@@ -1,7 +1,18 @@
+// The fetch handler handles incoming requests. 
+// It receives a Request object and returns a Response or 
+// Promise<Response>.
+
 const server = Bun.serve({
     port: 3000,
     fetch(req){
-        return new Response("Hello World! From a server!!!")
+        const url = new URL(req.url);
+        if (url.pathname === "/"){
+            return new Response("Home Page!");
+        }
+        if (url.pathname === "/about"){
+            return new Response("We are Tesseract Esports LLP!")
+        }
+        return new Response("Error 404");
     }
 })
 
